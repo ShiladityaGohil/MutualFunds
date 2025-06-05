77569f2ffa3206b79c4779b3cfaa27143cb03a1f
ShiladityasinhPE <shiladityasinh.gohil@pe.solutions>
2025-06-05 17:20:15 +0530

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -13,7 +13,7 @@ app.config.from_object(Config())
 
 scheduler = APScheduler()
 
-@scheduler.task('cron', id='update_funds_job', minutes=15)
+@scheduler.task('cron', id='update_funds_job', minute='15')
 def scheduled_task():
     print("Running update_funds...")
     update_funds.fetch_and_update(limit=None)

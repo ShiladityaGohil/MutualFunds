8fb1f8ebc3bc948225cb5a665678b3fe34957022
ShiladityasinhPE <shiladityasinh.gohil@pe.solutions>
2025-06-10 15:39:47 +0530

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -18,8 +18,7 @@ def scheduled_task_ping():
     print("Pinging server...")
     update_funds.ping_server()
 
-# @scheduler.task('cron', id='update_funds_job', hour='13',minute='58')
-@scheduler.task('cron', id='update_funds_job', hour='19',minute='33')
+@scheduler.task('cron', id='update_funds_job', hour='10',minute='13')
 def scheduled_task():
     print("Running update_funds...")
     update_funds.fetch_and_update(limit=None)
